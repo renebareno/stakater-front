ab7ce10eaac1c3dc8823ee6591e641a5c020fc7e
Rene Arturo Bareño Campos <rene.bareno@gmail.com>
2021-06-11 21:29:56 -0500

    react app, thah feth gretings api

---
+++ App.js
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from 'react';
+import { format } from 'date-fns';
+import './App.css';
+
+
+function App() {
+  const [appState, setAppState] = useState({
+    greeting: null, exception: null,
+  });
+
+  useEffect(() => {
+    let formatDateString = format(new Date(), 'dd/MM/yyyy kk:mm'); 
+    const apiUrl = `http://localhost:8080/greeting`;
+    fetch(apiUrl)
+      .then((res) => res.text())
+      .then((repos) => {
+        setAppState({ greeting: (formatDateString+ ' '+repos) });
+      }, (error) => {
+        if (error) {
+          setAppState({ greeting: ':(  There is an error' })
+        }
+      } );
+  }, [setAppState]);
+  return (
+    <div className="App">
+      <header className="App-header">
+
+        <p>
+          {appState.greeting}
+        </p>
+
+        <p>
+        {appState.exception}
+      </p>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
